bf38bf60ff78da6cd615632a3ef10db96025e062
Aaron Kable <aaronkable@gmail.com>
2012-09-09 11:45:41 +0800

    .net / c# code

---
+++ CoreTemp Module/ArduinoCoreTemp/Arduino_Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ArduinoCoreTempModule
+{
+    partial class Arduino_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arduino_Main));
+            this.cbCom = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.ACTnotf = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // cbCom
+            // 
+            this.cbCom.FormattingEnabled = true;
+            this.cbCom.Location = new System.Drawing.Point(13, 13);
+            this.cbCom.Name = "cbCom";
+            this.cbCom.Size = new System.Drawing.Size(121, 21);
+            this.cbCom.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Restart Com";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(12, 48);
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(260, 20);
+            this.tbConsole.TabIndex = 2;
+            // 
+            // ACTnotf
+            // 
+            this.ACTnotf.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.ACTnotf.Icon = ((System.Drawing.Icon)(resources.GetObject("ACTnotf.Icon")));
+            this.ACTnotf.Text = "notifyIcon1";
+            this.ACTnotf.Visible = true;
+            // 
+            // Arduino_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 75);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbCom);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Arduino_Main";
+            this.Text = "Arduino CoreTemp Module";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCom;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.NotifyIcon ACTnotf;
+    }
+}
+
---
+++ CoreTemp Module/ArduinoCoreTemp/Arduino_Main.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using System.IO.Ports;
+using GetCoreTempInfoNET;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ArduinoCoreTempModule
+{
+    public partial class Arduino_Main : Form
+    {
+        private System.Timers.Timer RefreshInfo;
+        private CoreTempInfo CTInfo;
+        private SerialPort comPort;
+        private ulong totalRam = 0;
+        //ram use
+        private Microsoft.VisualBasic.Devices.ComputerInfo CI = new Microsoft.VisualBasic.Devices.ComputerInfo();
+        //ram use
+        private ulong ramCounter; 
+
+        public ulong getAvailableRAM(){
+            return CI.AvailablePhysicalMemory / 1048576; 
+        } 
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                ACTnotf.Visible = true;
+                ACTnotf.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                ACTnotf.Visible = false;
+            }
+        }
+
+        public Arduino_Main()
+        {
+            InitializeComponent();
+
+            ACTnotf.BalloonTipText = "The Module is Still Running Find it Down Here!";
+            ACTnotf.BalloonTipTitle = "Arduino CoreTemp Module";
+            
+            // ui
+
+            // RAM
+            
+            totalRam = CI.TotalPhysicalMemory / 1048576;
+            
+            // Initiate CoreTempInfo class.
+            CTInfo = new CoreTempInfo();
+
+            // setup callbacks
+            CTInfo.ReportError += new ErrorOccured(CTInfo_ReportError);
+            ACTnotf.MouseDoubleClick += new MouseEventHandler(ACTnotf_Click);
+            this.FormClosing += new FormClosingEventHandler(frm_FormClosing);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+
+            // get stored com
+            String comHold = Properties.Settings.Default.comPort;
+            string[] ports = SerialPort.GetPortNames();
+            
+            // set the CB items
+            cbCom.Items.AddRange(ports);
+
+            // create and open com
+            comPort = new SerialPort(comHold, 19200);
+            try
+            {
+                comPort.Open();
+            }
+            catch (Exception expt)
+            {
+                // not avail main thread will deal with it
+            }
+
+            // set the combo box to the prev item
+            cbCom.SelectedItem = comHold;
+
+            // setup a timer.
+            RefreshInfo = new System.Timers.Timer();
+            RefreshInfo.Interval = 600;
+            // setup callback
+            RefreshInfo.Elapsed += new ElapsedEventHandler(RefreshInfo_Elapsed);
+
+            // Start Main thread.
+            RefreshInfo.Start();
+
+        }
+        // from the core temp demo code 
+        private void RefreshInfo_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //Attempt to read shared memory.
+            bool bReadSuccess = CTInfo.GetData();
+
+            if (bReadSuccess)
+            {
+                uint index;
+                float maxT = 1.0f;
+                int load = 0;
+                // get max and avg load for all cpu's + cores
+                for (uint i = 0; i < CTInfo.GetCPUCount; i++)
+                {
+                    for (uint g = 0; g < CTInfo.GetCoreCount; g++)
+                    {
+                        index = g + (i * CTInfo.GetCoreCount);
+                        if (!CTInfo.IsDistanceToTjMax)
+                        {
+                            if (maxT <= CTInfo.GetTemp[index])
+                            {
+                                maxT = CTInfo.GetTemp[index];
+                            }
+                            load += (int)CTInfo.GetCoreLoad[index];
+                        }
+                    }
+                }
+
+                //date string
+                string datePatt = @"HH:mm";
+                DateTime saveNow = DateTime.Now;
+                DateTime myDt;
+                myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Local);
+                string dtString;
+
+
+                // setup output string
+                // Core Speed, Core Temp, Max Core Temp, Time, Average Load, 
+                // #.#Ghz,#.#,##:##,###
+
+                dtString = myDt.ToString(datePatt);
+                String output = Math.Round((CTInfo.GetCPUSpeed / 1000), 1) + "Ghz" + "," + Math.Round(maxT, 1) + "," + dtString + "," + +((int)(load / CTInfo.GetCoreCount));
+                //update tooltip icon
+                ACTnotf.Text = "Module Running - " + output;
+                
+                // write com if avail if not notify
+                if (comPort.IsOpen)
+                {
+                    comPort.WriteLine(output);
+                    try
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        { // added ram to display not to string as arduino is not updated to show it yet.
+                            tbConsole.Text = output + "," + getAvailableRAM() + "," + totalRam + "," + ((getAvailableRAM() * 100 / totalRam))+"%"; // runs on UI thread 
+                        });
+                    }
+                    catch (Exception excp)
+                    {
+
+                    }
+                }
+                else // no com available
+                {
+                    try
+                    {
+                        this.Invoke((MethodInvoker)delegate
+                        {
+                            tbConsole.Text = "Com port not available"; // runs on UI thread
+                        });
+                    }
+                    catch (Exception excp)
+                    {
+
+                    }
+                }
+                            
+            }
+            else // coretemp not avail
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    tbConsole.Text = CTInfo.GetErrorMessage(CTInfo.GetLastError); // runs on UI thread
+                });
+                
+            }
+
+        }
+
+        //error handler not used
+        private void CTInfo_ReportError(ErrorCodes ErrCode, string ErrMsg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ErrMsg);
+            Console.ResetColor();
+        }
+
+        // reset com button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.comPort = "" + cbCom.SelectedItem;
+            Properties.Settings.Default.Save();
+
+            if (comPort.IsOpen)
+            {
+                comPort.Close();
+            }
+            comPort = new SerialPort(("" + cbCom.SelectedItem), 19200);
+            try
+            {
+                comPort.Open();
+            }
+            catch (Exception expt)
+            {
+                // not avail main thread will deal with it
+            }
+        }
+
+        // minimise to tray
+        private void ACTnotf_Click(object sender, EventArgs e)
+        {
+            ACTnotf.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        // minimise to tray
+        private void frm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close?\nSerial Communications will be closed", "Arduino CoreTemp", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (comPort.IsOpen)
+                {
+                    comPort.Close();
+                }
+                ACTnotf.Visible = false;
+            }
+        } 
+      
+    }
+}
